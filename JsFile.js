45df5d154d5bd634f3dd3fbb7518c017b4a8dfec
earthspacon <47030149+earthspacon@users.noreply.github.com>
2019-04-29 12:08:51 +0500

    Add files via upload

---
+++ JsFile.js
@@ -0,0 +1,8 @@
+
+alert('Hi, are you really learing web?');
+var smth = prompt('Сколько вам лет? ', '');
+if(smth==17){
+    alert('Мы ровестники.');
+}
+else { alert(' Поздравляем, ваш возраст стал выигрышным!\n И вы выиграли новый Iphone X.\n Поздравляем!');
+}
